cbec2b71fb4d20c55df850ac37c512208dd11a34
nuleongee <nuleongee@gmail.com>
2022-02-15 23:58:17 +0900

    Update generator odds

--- ES6/generator.js
+++ ES6/generator.js
@@ -17,3 +17,45 @@ console.log(iter.next());
 console.log(iter.next());
 
 for (const a of gen()) console.log(a);
+
+/* odds */
+
+/**
+ * @param i : 시작 값
+ * @returns {Generator<number, void, *>}
+ */
+function* infinity(i = 0) {
+  while (true) yield i++;
+}
+
+/**
+ * @param l : 마지막 값
+ * @param iter : 받은 이터러블
+ * @returns {Generator<*, void, *>}
+ */
+function* limit(l, iter) {
+  for (const a of iter) {
+    yield a;
+    if (a === l) return;
+  }
+}
+
+/**
+ * @param l : 마지막 값
+ * @returns {Generator<*, void, *>}
+ */
+function* odds(l) {
+  for (const a of limit(l, infinity(1))) {
+    if (a % 2) yield a;
+  }
+}
+let iter2 = odds(10);
+console.log(iter2.next());
+console.log(iter2.next());
+console.log(iter2.next());
+console.log(iter2.next());
+console.log(iter2.next());
+console.log(iter2.next());
+console.log(iter2.next());
+
+for (const a of odds(40)) console.log(a);
