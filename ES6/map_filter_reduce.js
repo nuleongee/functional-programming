ed862129303754276c6dc3e18dfa5a17b7659c29
Nuleongee <nuleongee@gmail.com>
2022-02-17 11:56:10 +0900

    Update map

--- ES6/map_filter_reduce.js
+++ ES6/map_filter_reduce.js
@@ -28,3 +28,23 @@ for (const p of products) {
 }
 console.log(prices);
 console.log(map(p => p.price, products)); // map 사용
+
+/* 이터러블 프로토콜에 따른 map의 다형성 */
+// console.log(document.querySelectorAll('*'));
+
+function* gen() {
+  yield 2;
+  if (false) yield 3;
+  yield 4;
+}
+console.log(map(a => a * a, gen()));
+
+let m = new Map();
+m.set('a', 10);
+m.set('b', 20);
+// const it = m[Symbol.iterator]();
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+console.log(map(([k, a]) => [k, a * 2], m));
+console.log(new Map(map(([k, a]) => [k, a * 2], m)));
