204f4127b593c6ffdfda85aad2aeb5476b2b41cb
nuleongee <nuleongee@gmail.com>
2022-02-18 22:55:35 +0900

    Update filter

--- ES6/map_filter_reduce.js
+++ ES6/map_filter_reduce.js
@@ -48,3 +48,33 @@ m.set('b', 20);
 // console.log(it.next());
 console.log(map(([k, a]) => [k, a * 2], m));
 console.log(new Map(map(([k, a]) => [k, a * 2], m)));
+
+/* filter */
+// let over20000 = [];
+// for (const p of products) {
+//   if (p.price < 20000) over20000.push(p);
+// }
+// console.log(...over20000);
+
+const filter = (f, iter) => {
+  let res = [];
+  for (const a of iter) {
+    if (f(a)) res.push(a);
+  }
+  return res;
+};
+console.log(...filter(p => p.price < 20000, products));
+
+console.log(filter(n => n % 2, [1, 2, 3, 4, 5]));
+console.log(
+  filter(
+    n => n % 2,
+    (function* () {
+      yield 1;
+      yield 2;
+      yield 3;
+      yield 4;
+      yield 5;
+    })(),
+  ),
+);
