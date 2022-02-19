3e31b192a0694935ce7108d775f9fb949b878ffd
nuleongee <nuleongee@gmail.com>
2022-02-19 23:55:05 +0900

    Update reduce

--- ES6/map_filter_reduce/reduce.js
+++ ES6/map_filter_reduce/reduce.js
@@ -23,3 +23,13 @@ const add = (a, b) => a + b;
 
 console.log(reduce(add, 0, [1, 2, 3, 4, 5])); // 15
 console.log(reduce(add, [1, 2, 3, 4, 5])); // 15
+
+const products = [
+  { name: '반팔티', price: 15000 },
+  { name: '목폴라', price: 19000 },
+  { name: '후리스', price: 79000 },
+  { name: '후드티', price: 48000 },
+  { name: '청바지', price: 56000 },
+];
+
+console.log(reduce((total_price, product) => total_price + product.price, 0, products));
